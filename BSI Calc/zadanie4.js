a692112d1efec2a6eda688a89c410441cf53f4cb
Dudziej <46353850+Dudziej@users.noreply.github.com>
2020-11-11 19:38:55 +0100

    Maciej Dudzik zad 4, 5 & index upload
    2 calculators and forms added

--- BSI Calc/zadanie4.js
+++ BSI Calc/zadanie4.js
@@ -1,1 +1,9 @@
-function zad4(){}
+function zad4(){
+    var x = document.getElementById('x').value;
+    var t = document.getElementById('t').value;   
+    myResult = Math.exp(-(x*t));
+    var zad5form = document.getElementById("zad4form");
+    var z = document.createTextNode("Reliability: "+myResult.toFixed(4)*100+"%");
+    zad4form.appendChild(document.createElement("br"));
+    zad4form.appendChild(z);
+}
--- BSI Calc/zadanie5.js
+++ BSI Calc/zadanie5.js
@@ -1,1 +1,9 @@
-function zad5(){}
+function zad5(){
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;   
+    var myResult2 =Math.pow((1-a/100), b);
+    var zad5form = document.getElementById("zad5form");
+    var w = document.createTextNode("Probability: "+myResult2.toFixed(2)*100+"%");
+    zad5form.appendChild(document.createElement("br"));
+    zad5form.appendChild(w);
+}
